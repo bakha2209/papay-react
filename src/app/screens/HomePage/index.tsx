f61bffecd4be3fb33b0ea11e9aeb259171166be2
bakha2209 <bakhodir2209@gmail.com>
2023-11-16 12:24:33 +0900

    fix: develop homepage components

--- src/app/screens/HomePage/index.tsx
+++ src/app/screens/HomePage/index.tsx
@@ -1,6 +1,20 @@
 import { Container } from "@mui/material";
 import React from "react";
+import { Statistics } from "./statistics";
+import { TopRestaurants } from "./topRestaurants";
+import { BestRestaurants } from "./bestRestaurants";
+import { BestDishes } from "./bestDishes";
+import { Advertisements } from "./advertisements";
+import { Events } from "./events";
+import "../../../css/home.css";
 
 export function HomePage() {
-    return <Container>HomePage</Container>
+    return <div className="homepage">
+        <Statistics />
+        <TopRestaurants/>
+        <BestRestaurants/>
+        <BestDishes/>
+        <Advertisements/>
+        <Events/>
+    </div>
 }
---
+++ src/app/screens/HomePage/statistics.tsx
@@ -0,0 +1,35 @@
+import { Box, Container, Stack } from "@mui/material";
+import React from "react";
+import Marginer from "../../components/marginal";
+
+export function Statistics() {
+  return (
+    <div className="static_frame">
+      <Container>
+        <Stack className="stat_stack">
+          <div className="personage_img left"></div>
+          <Stack className="static_box">
+            <Box className="static_num">12</Box>
+            <Box className="static_text">Restaurants</Box>
+          </Stack>
+          <Marginer direction="vertical" height="64" width="2" bg="#E3C080"/>
+          <Stack className="static_box">
+            <Box className="static_num">8</Box>
+            <Box className="static_text">Years Experience</Box>
+          </Stack>
+          <Marginer direction="vertical" height="64" width="2" bg="#E3C080"/>
+          <Stack className="static_box">
+            <Box className="static_num">50+</Box>
+            <Box className="static_text">Menu ovqatlar</Box>
+          </Stack>
+          <Marginer direction="vertical" height="64" width="2" bg="#E3C080"/>
+          <Stack className="static_box">
+            <Box className="static_num">200+</Box>
+            <Box className="static_text">Foydalanuvchilar</Box>
+          </Stack>
+          <div className="personage_img right"></div>
+        </Stack>
+      </Container>
+    </div>
+  );
+}
