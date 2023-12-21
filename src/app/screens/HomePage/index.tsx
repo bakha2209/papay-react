77f0e26d1251517b67dc7a1e0b7399df327b20b8
bakha2209 <bakhodir2209@gmail.com>
2023-12-21 14:36:54 +0900

    feat: usage of slice and selecters on homePage component

--- src/app/screens/HomePage/index.tsx
+++ src/app/screens/HomePage/index.tsx
@@ -9,15 +9,39 @@ import { Events } from "./events";
 import "../../../css/home.css";
 import { Recommendations } from "./recommendations";
 
-export function HomePage() {
+//REDUX
+import { useDispatch, useSelector } from "react-redux";
+import { Dispatch } from "@reduxjs/toolkit";
+import { createSelector } from "reselect";
+import { setTopRestaurants } from "./slice";
+import { retrieveTopRestaurants } from "./selector";
+import { Restaurant } from "../../../types/user";
 
+// REDUX SLICE
+const actionDispatch = (dispach: Dispatch) => ({
+  setTopRestaurants: (data: Restaurant[]) => dispach(setTopRestaurants(data)),
+});
+// REDUX SELECTOR
+const topRestaurantRetriever = createSelector(
+  retrieveTopRestaurants,
+  (topRestaurants) => ({
+    topRestaurants,
+  })
+);
+
+export function HomePage() {
+  //INITIALIZATION
+  const { setTopRestaurants } = actionDispatch(useDispatch());
+  const { topRestaurants } = useSelector(topRestaurantRetriever);
   //selector: store => data
 
   useEffect(() => {
     //backend data request => data
+    
+    setTopRestaurants([])
 
     //slice: data => store
-  }, [])
+  }, []);
   return (
     <div className="homepage">
       <Statistics />
