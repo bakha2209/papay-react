9ba95d8e426d6d0c281e5918430ea7e5946cab4c
bakha2209 <bakhodir2209@gmail.com>
2024-01-06 13:46:43 +0900

    feat: organize retrieve of memberPage url query data

--- src/app/screens/MemberPage/index.tsx
+++ src/app/screens/MemberPage/index.tsx
@@ -1,25 +1,33 @@
 import { Container } from "@mui/material";
 import React from "react";
-import { Route, Switch, useRouteMatch } from "react-router-dom";
+import { Route, Switch, useRouteMatch, useLocation } from "react-router-dom";
 import { VisitOtherPage } from "./VisitOtherPage";
 import { VisitMyPage } from "./VisitMyPage";
-import "../../../css/my_page.css"
+import "../../../css/my_page.css";
+
+function useQuery() {
+  const { search } = useLocation();
+  return React.useMemo(() => new URLSearchParams(search), [search]);
+}
 
 export function MemberPage(props: any) {
-    const {verifiedMemberData} = props
-    let member = useRouteMatch();
-    console.log(member);
-    return (
-      <div className="restaurant_page">
-        <Switch>
-          <Route path={`${member.path}/other`}>
-            <VisitOtherPage verifiedMemberData={verifiedMemberData}/>
-          </Route>
-          <Route path={`${member.path}`}>
-            <VisitMyPage verifiedMemberData={verifiedMemberData}/>
-          </Route>
-          
-        </Switch>
-      </div>
-    );
+  const { verifiedMemberData } = props;
+  let member = useRouteMatch();
+  const query = useQuery();
+  const chosen_mb_id: string|null=query.get("mb_id") ?? null
+  const chosen_art_id: string|null = query.get("art_id") ?? null
+  
+  console.log("query test", query.get("test"));
+  return (
+    <div className="restaurant_page">
+      <Switch>
+        <Route path={`${member.path}/other`}>
+          <VisitOtherPage verifiedMemberData={verifiedMemberData} />
+        </Route>
+        <Route path={`${member.path}`}>
+          <VisitMyPage verifiedMemberData={verifiedMemberData} />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
