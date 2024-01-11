5361810b487592399f8d5f2aae199f4692999282
bakha2209 <bakhodir2209@gmail.com>
2024-01-11 16:01:21 +0900

    feat: develop Socket.io client fundamental logic

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,27 @@
 import React from "react";
-import ReactDOM from "react-dom"
+import ReactDOM from "react-dom";
 import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
+import { Provider } from 'react-redux';
 import { store } from "./app/store";
 import App from "./app/App";
 import reportWebVitals from "./reportWebVitals";
 import "./css/index.css";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
-import theme from "./app/MaterialTheme"
+import theme from "./app/MaterialTheme";
 
-
+import { SocketContext, socket } from "./app/context/socket";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <App />
+        <SocketContext.Provider value={socket}>
+          <App />
+        </SocketContext.Provider>
+          
+        
       </ThemeProvider>
     </Provider>
   </React.StrictMode>,
